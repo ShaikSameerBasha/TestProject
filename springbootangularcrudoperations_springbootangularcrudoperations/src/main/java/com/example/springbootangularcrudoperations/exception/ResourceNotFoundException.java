bd90e594f5bd222e88a3242014af55e269a144ea
ShaikSameerBasha <97223532+ShaikSameerBasha@users.noreply.github.com>
2024-04-29 12:34:31 +0530

    Delete springbootangularcrudoperations_springbootangularcrudoperations directory

--- springbootangularcrudoperations_springbootangularcrudoperations/src/main/java/com/example/springbootangularcrudoperations/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootangularcrudoperations.exception;
-
-public class ResourceNotFoundException  extends Exception{
-
-	public ResourceNotFoundException(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
